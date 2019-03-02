46675cb0715c55d3aacec3deb3fdff7e361ae6bc
Haamid <ahaamid3@gatech.edu>
2019-03-02 12:25:02 -0500

    removed the code for the first three calls

--- PTTBackend2/src/controllers/UserController.ts
+++ PTTBackend2/src/controllers/UserController.ts
@@ -12,27 +12,7 @@ export class UserController {
     public addUser(userJSON) {
         return new promise<Result> ((resolve, reject) => {
             try {
-                delete userJSON.id; // ignoring the id field set by the frontend
-                let newUser = new this.User(userJSON);
-                newUser.save((error, user) => {
-                    if (error) {
-                        if (error.name === 'MongoError') {
-                            if (error.code === 11000) {
-                                print("duplicate emails");
-                                reject({code: 409, result: error.errmsg});
-                            } else {
-                                print("unknown MongoError:", error);
-                                reject({code: 500, result: "Server error"});
-                            }
-                        } else if (error.name === 'ValidationError') {
-                            print("ValidationError:", error._message);
-                            reject({code: 400, result: error._message});
-                        }
-                    } else {
-                        user = moldJSON(user);
-                        resolve({code: 201, result: user});
-                    }
-                });
+                
             } catch (e) {
                 print("500: server error:", e);
                 reject({code: 500, result: "Server error"});
@@ -43,20 +23,7 @@ export class UserController {
     public getUser(userId: string) {
         return new promise<Result> ((resolve, reject) => {
             try {
-                this.User.findById(userId, (err: any, user: mongoose.Document) => {
-                    if (err) {
-                        print("err:", err);
-                        reject({code: 400, result: "Bad request"});
-                    } else {
-                        if (user) {
-                            user = moldJSON(user);
-                            resolve({code: 200, result: user});
-                        } else {
-                            print("User not found:", userId);
-                            reject({code: 404, result: "User not found"});
-                        }
-                    }
-                });
+                
             } catch (e) {
                 print("500: server error:", e);
                 reject({code: 500, result: "Server error"});
@@ -67,23 +34,7 @@ export class UserController {
     public updateUser(userId: string, updatedUser) {
         return new promise<Result> ((resolve, reject) => {
             try {
-                delete updatedUser.email; // ignoring any update to the email set by the frontend since modifying email is not allowed
-                let condition = { _id: { $eq: userId } };
-                let options = {new: true};
-                this.User.findOneAndUpdate(condition, updatedUser, options, (err: any, res: mongoose.Document) => {
-                    if (err) {
-                        print("err:", err.message, err);
-                        reject({code: 400, result: err.message});
-                    } else {
-                        if (res) {
-                            res = moldJSON(res);
-                            resolve({code: 200, result: res});
-                        } else {
-                            print("User not found:", userId);
-                            reject({code: 404, result: "User not found"});
-                        }
-                    }
-                })
+                
             } catch (e) {
                 print("500: server error:", e);
                 reject({code: 500, result: "Server error"});
@@ -95,20 +46,7 @@ export class UserController {
         print(userId);
         return new promise<Result> ((resolve, reject) => {
             try {
-                this.User.findByIdAndDelete(userId, (err: any, user: mongoose.Document) => {
-                    if (err) {
-                        print("err:", err.message);
-                        reject({code: 400, result: err.message});
-                    } else {
-                        if (user) {
-                            user = moldJSON(user);
-                            resolve({code: 200, result: user});
-                        } else {
-                            print("User not found:", userId);
-                            reject({code: 404, result: `User not found`});
-                        }
-                    }
-                });
+                
             } catch (e) {
                 print("500: server error:", e);
                 reject({code: 500, result: "Server error"});
