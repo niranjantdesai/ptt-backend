3029b5473a4d8e483abc8944eb14e454a8be4212
Weihua Zhu <zhuweihua2746@gmail.com>
2019-03-25 15:59:20 -0400

    Fixed all updateUser tests for Mobile1 Team backend tests

--- PTTBackend2/src/test/java/edu/gatech/cs6301/BackendTestsMobile1.java
+++ PTTBackend2/src/test/java/edu/gatech/cs6301/BackendTestsMobile1.java
@@ -117,7 +117,7 @@ public class BackendTestsMobile1 {
 
             System.out.println("*** String response " + strResponse + " (" + response.getStatusLine().getStatusCode() + ") ***");
 
-            String expectedJson = "{\"id\":" + id + ",\"firstName\":\"Tom\",\"lastName\":\"Doe\",\"email\":\"john@doe.org\"}";
+            String expectedJson = "{\"id\":" + id + ",\"firstName\":\"Tom\",\"lastName\":\"Doe\",\"email\":\"tom@doe.org\"}";
 	        JSONAssert.assertEquals(expectedJson,strResponse, false);
             EntityUtils.consume(response.getEntity());
             response.close();
